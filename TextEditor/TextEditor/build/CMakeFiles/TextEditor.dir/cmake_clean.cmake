652f10c5bb0bedab2d4aee95fc6244fe8856a00c
Smallgigach <artyom.s.2020@gmail.com>
2025-05-14 12:34:17 +0300

    init

---
+++ TextEditor/TextEditor/build/CMakeFiles/TextEditor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextEditor.dir/link.d"
+  "CMakeFiles/TextEditor.dir/main.cpp.o"
+  "CMakeFiles/TextEditor.dir/main.cpp.o.d"
+  "TextEditor"
+  "TextEditor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TextEditor/TextEditor/build/CMakeFiles/TextEditor.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/TextEditor.dir/main.cpp.o: \
+ /home/smallgigach/Projects/TextEditor/TextEditor/TextEditor/main.cpp \
+ /usr/include/stdc-predef.h
